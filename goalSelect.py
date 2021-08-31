b766c2dd827960783b40798cfa50877af95a438b
Mohammed Abdullhak <m_abdulhak@outlook.com>
2021-08-31 20:20:32 -0230

    Refactor goalSelect

--- goalSelect.py
+++ goalSelect.py
@@ -27,93 +27,15 @@ def updateGoal(controller, robotPosition, bvcCell, sensor_data, env):
 
   def getGoalFromClosestPointToEnvBounds():
     global curGoalTimeSteps
+    log("Getting goal from Orbit Map")
 
-    log("Getting goal from closest point to env bounds")
-
-    # len = distanceBetween2Points(robotPosition, closestPoint)
-
-    # if (len == 0):
-    #   len = 0.01
-
-    # translationVec = { \
-    #   "x": ((closestPoint["x"] - robotPosition["x"]) * robotRadius) / (len * 10), \
-    #   "y": ((closestPoint["y"] - robotPosition["y"]) * robotRadius) / (len * 10), \
-    # }
-
-    # midPoint = translatePointInDirection( \
-    #   robotPosition["x"], \
-    #   robotPosition["y"], \
-    #   translationVec["x"], \
-    #   translationVec["y"])
-
-    # delta = robotRadius * 2
-    # newGoal = midPoint
-
-    # newGoal = shiftPointOfLineSegInDirOfPerpendicularBisector( \
-    #   midPoint["x"], \
-    #   midPoint["y"], \
-    #   robotPosition["x"], \
-    #   robotPosition["y"], \
-    #   closestPoint["x"], \
-    #   closestPoint["y"], \
-    #   delta)
-
-    # newGoalPoint = Point(newGoal["x"], newGoal["y"])
-    # condGoalNotReachable = newGoalPoint.distance(env.boundary) < robotRadius
-    
-    # log("New Goal:", newGoalPoint.wkt, \
-    #   "Env:", env.wkt, \
-    #   "Dist from Point to new goal:", newGoalPoint.distance(env.boundary), \
-    #   "Dist from Env bounds to new goal:", env.exterior.distance(newGoalPoint), \
-    #   "New goal not Valid (dist < robotRadius)?", condGoalNotReachable)
-
-    # if (condGoalNotReachable):
-    #   log("Goal Too Close to Edge Detected!")
-    #   log("Old translation vector:", translationVec)
-    #   translationVec["x"] *= -1
-    #   translationVec["y"] *= -1
-    #   log("New translation vector:", translationVec)
-
-    #   midPoint = translatePointInDirection( \
-    #     robotPosition["x"], \
-    #     robotPosition["y"], \
-    #     translationVec["x"], \
-    #     translationVec["y"])
-
-    #   newGoal = midPoint
-
-    #   newGoal = shiftPointOfLineSegInDirOfPerpendicularBisector(
-    #     midPoint["x"],
-    #     midPoint["y"],
-    #     robotPosition["x"],
-    #     robotPosition["y"],
-    #     closestPoint["x"],
-    #     closestPoint["y"],
-    #     delta)
-      
-    #   log("Modified new goal:", newGoal)
-
-    # if (not pointIsInsidePolygon(newGoal, env)):
-    #   log("GOAL OUTSIDE ENV DETECTED:")
-    #   log("Old Goal", newGoal)
-    #   newGoal = {
-    #     "x": robotPosition["x"] + 10 * (robotPosition["x"] - closestPoint["x"]),
-    #     "y": robotPosition["y"] + 10 * (robotPosition["y"] - closestPoint["y"])
-    #   }
-    #   log("New Goal", newGoal)
-    #   # exit()
-      
-
-    
     x = int(robotPosition["x"] * MAP_SCALE)
     y = int(robotPosition["y"] * MAP_SCALE)
     goal = ENV_MAP[y][x]
     curGoalTimeSteps = 0
     return {"x": goal[0] / MAP_SCALE, "y": goal[1] / MAP_SCALE}
 
-    return newGoal
-
-  def getRandGoal():
+  def getGoalFromOrbit():
     global curGoalTimeSteps
 
     log("Getting goal from Env!")
@@ -127,17 +49,6 @@ def updateGoal(controller, robotPosition, bvcCell, sensor_data, env):
 
     curPosPoint = Point(robotPosition["x"], robotPosition["y"])
 
-    # closestPointToEnvBounds, p2 = nearest_points(env.boundary, curPosPoint)    
-    # closestPoint = closestPointToEnvBounds
-
-    # IMP: orbit static obstacles as well as env
-    # closestPointToStaticObstacles = getClosesetPointOfStaticObstacles(sensor_data, controller.staticObstacles)
-    # if(curPosPoint.distance(closestPointToStaticObstacles) < curPosPoint.distance(closestPointToEnvBounds)):
-    #   closestPoint = closestPointToStaticObstacles
-    #   print("Static Obstacles closer!", closestPointToStaticObstacles.wkt, closestPointToEnvBounds.wkt)
-      # exit()
-
-    # log("closestPoint", closestPointToEnvBounds.wkt)
     newGoal = getGoalFromClosestPointToEnvBounds()
     log("Final selected new goal:", newGoal)
 
@@ -152,7 +63,7 @@ def updateGoal(controller, robotPosition, bvcCell, sensor_data, env):
     if (normalizedAngle < ACCEPTABLE_PUCK_ANGLE_THRESH):
       return closestPointInLine
 
-    return getRandGoal()
+    return getGoalFromOrbit()
 
   def selectBestNearbyPuck():
     global curGoalTimeSteps
@@ -202,8 +113,6 @@ def updateGoal(controller, robotPosition, bvcCell, sensor_data, env):
       bestPuck = angleRatings[0]
     # elif (distRatsExist and distanceRatings[0][1]  < robotRadius * 5):
     #   bestPuck = distanceRatings[0]
-    # elif (angleRatsExist):
-    #   bestPuck = angleRatings[0]
 
     if (bestPuck != None):
       curGoalTimeSteps = 0
@@ -233,7 +142,7 @@ def updateGoal(controller, robotPosition, bvcCell, sensor_data, env):
   log("bestPuck:",bestPuck)
   # exit()
   if (bestPuck == None):
-    goal = getRandGoal()
+    goal = getGoalFromOrbit()
     goal = limitGoalWithinEnvOutsideStaicObstacles(goal)
     return goal
   else:
