355d96864736a95c63e899bf42f5b5266c84c4c3
Mohammed Abdullhak <m_abdulhak@outlook.com>
2021-09-13 14:44:27 -0230

    Refactor generateOrbitMap

--- generateOrbitMap.py
+++ generateOrbitMap.py
@@ -13,13 +13,11 @@ def pointIsInsidePolygon(point, poly):
         return False
 
 def readConfig(filePath):
-    f = open(filePath)
-    data = json.load(f)
-    f.close()
-    return data
+    with open(filePath) as f:
+        return json.load(f)
 
 MIN_DIRECTION = 30
-ENV_ORBIT_STEEPNESS = 2
+ENV_ORBIT_STEEPNESS = 14
 INCLUDE_OBSTACLES = True
 SAVE_FIGURES = True
 SHOW_FIGURES = False
@@ -38,13 +36,14 @@ env = config["env"]
 obs = config["staticObstacles"]
 
 envPoly = Polygon([p[0] * MAP_SCALE, p[1] * MAP_SCALE] for p in env)
-envXMax = int(max([x[0] for x in env]) * MAP_SCALE) 
-envYMax = int(max([x[1] for x in env]) * MAP_SCALE)
+envXMax = int(max(x[0] for x in env) * MAP_SCALE)
+envYMax = int(max(x[1] for x in env) * MAP_SCALE)
 print("Env:", env, " Max X:", envXMax,"Max Y:", envYMax)
 
-staticObstaclesPolygons = []
-for o in obs:
-    staticObstaclesPolygons.append(Polygon([p[0] * MAP_SCALE, p[1] * MAP_SCALE] for p in o))
+staticObstaclesPolygons = [
+    Polygon([p[0] * MAP_SCALE, p[1] * MAP_SCALE] for p in o) for o in obs
+]
+
 print("Obs:", {ob.wkt for ob in staticObstaclesPolygons})
 
 ###########################################
@@ -55,12 +54,12 @@ X, Y = np.meshgrid(np.linspace(0, envXMax, envXMax+1), np.linspace(0, envYMax, e
 map_border_mask = -1*np.ones_like(X)
 speed = 1*np.ones_like(X)
 
-for x in range(0, envXMax):
-    for y in range(0, envYMax):
+for x in range(envXMax):
+    for y in range(envYMax):
         curPoint = Point(x,y)
         if(curPoint.within(envPoly) and envPoly.exterior.distance(curPoint) > CLEARANCE):
             map_border_mask[y][x] = 1
-        
+
         if(INCLUDE_OBSTACLES):
             for o in staticObstaclesPolygons:
                 if(curPoint.distance(o) < CLEARANCE):
@@ -73,83 +72,79 @@ for x in range(0, envXMax):
 t = skfmm.travel_time(map_border_mask, speed, 1)
 t_filled = t
 
-yG, xG = np.gradient(t_filled)
-x_directions = -10 * xG
-y_directions = -10 * yG
+gradients_y, gradients_x = np.gradient(t_filled)
+vec_to_border_x = -10 * gradients_x
+vec_to_border_y = -10 * gradients_y
 
 ###########################################
 ####    Generate Environment Orbits    ####
 ###########################################
-x_directions_shifted = ENV_ORBIT_STEEPNESS * -1 *  y_directions + x_directions
-y_directions_shifted = ENV_ORBIT_STEEPNESS * x_directions + y_directions
+orbit_vec_x = ENV_ORBIT_STEEPNESS * -1 *  vec_to_border_y + vec_to_border_x
+orbit_vec_y = ENV_ORBIT_STEEPNESS * vec_to_border_x + vec_to_border_y
 
 def vecLength(x, y):
     return sqrt(x*x + y*y)
 
-for x in range(0, envXMax):
-    for y in range(0, envYMax):
+for x in range(envXMax):
+    for y in range(envYMax):
         curPoint = Point(x,y)
         if (map_border_mask[y][x] != -1):
-            x_directions_shifted[y][x] = ENV_ORBIT_STEEPNESS * y_directions[y][x] + x_directions[y][x]
-            y_directions_shifted[y][x] = ENV_ORBIT_STEEPNESS * -1 * x_directions[y][x] + y_directions[y][x]
+            orbit_vec_x[y][x] = ENV_ORBIT_STEEPNESS * vec_to_border_y[y][x] + vec_to_border_x[y][x]
+            orbit_vec_y[y][x] = ENV_ORBIT_STEEPNESS * -1 * vec_to_border_x[y][x] + vec_to_border_y[y][x]
 
-        if (x_directions_shifted[y][x] >= 0 and x_directions_shifted[y][x] < MIN_DIRECTION):
-            x_directions_shifted[y][x]  = MIN_DIRECTION
-        if (x_directions_shifted[y][x] <= 0 and x_directions_shifted[y][x] > -1 * MIN_DIRECTION):
-            x_directions_shifted[y][x]  = -1 * MIN_DIRECTION
-        if (y_directions_shifted[y][x] >= 0 and y_directions_shifted[y][x] < MIN_DIRECTION):
-            y_directions_shifted[y][x]  = MIN_DIRECTION
-        if (y_directions_shifted[y][x] <= 0 and y_directions_shifted[y][x] > -1 * MIN_DIRECTION):
-            y_directions_shifted[y][x]  = -1 * MIN_DIRECTION
+        if (orbit_vec_x[y][x] >= 0 and orbit_vec_x[y][x] < MIN_DIRECTION):
+            orbit_vec_x[y][x]  = MIN_DIRECTION
+        if (orbit_vec_x[y][x] <= 0 and orbit_vec_x[y][x] > -1 * MIN_DIRECTION):
+            orbit_vec_x[y][x]  = -1 * MIN_DIRECTION
+        if (orbit_vec_y[y][x] >= 0 and orbit_vec_y[y][x] < MIN_DIRECTION):
+            orbit_vec_y[y][x]  = MIN_DIRECTION
+        if (orbit_vec_y[y][x] <= 0 and orbit_vec_y[y][x] > -1 * MIN_DIRECTION):
+            orbit_vec_y[y][x]  = -1 * MIN_DIRECTION
 
 
 from scipy.ndimage.filters import gaussian_filter
 
-x_directions_shifted = gaussian_filter(x_directions_shifted, sigma=2)
-y_directions_shifted = gaussian_filter(y_directions_shifted, sigma=2)
+orbit_vec_x = gaussian_filter(orbit_vec_x, sigma=2)
+orbit_vec_y = gaussian_filter(orbit_vec_y, sigma=2)
 
 countAll = 0
 sumAll = 0
 minAll = None
 
-for x in range(0, envXMax):
-    for y in range(0, envYMax):
-        curPoint = Point(x,y)        
-        vLength = vecLength(x_directions_shifted[y][x], y_directions_shifted[y][x])
+for x in range(envXMax):
+    for y in range(envYMax):
+        curPoint = Point(x,y)
+        vLength = vecLength(orbit_vec_x[y][x], orbit_vec_y[y][x])
 
-        if (vLength < MIN_DIRECTION / 2 and x_directions_shifted[y][x] >= 0 and x_directions_shifted[y][x] < MIN_DIRECTION):
-            x_directions_shifted[y][x]  = MIN_DIRECTION / 2
-        if (vLength < MIN_DIRECTION / 2 and x_directions_shifted[y][x] <= 0 and x_directions_shifted[y][x] > -1 * MIN_DIRECTION):
-            x_directions_shifted[y][x]  = -1 * MIN_DIRECTION / 2
-        if (vLength < MIN_DIRECTION / 2 and y_directions_shifted[y][x] >= 0 and y_directions_shifted[y][x] < MIN_DIRECTION):
-            y_directions_shifted[y][x]  = MIN_DIRECTION / 2
-        if (vLength < MIN_DIRECTION / 2 and y_directions_shifted[y][x] <= 0 and y_directions_shifted[y][x] > -1 * MIN_DIRECTION):
-            y_directions_shifted[y][x]  = -1 * MIN_DIRECTION / 2
-            
-        vLength = vecLength(x_directions_shifted[y][x], y_directions_shifted[y][x])
+        if (vLength < MIN_DIRECTION / 2 and orbit_vec_x[y][x] >= 0 and orbit_vec_x[y][x] < MIN_DIRECTION):
+            orbit_vec_x[y][x]  = MIN_DIRECTION / 2
+        if (vLength < MIN_DIRECTION / 2 and orbit_vec_x[y][x] <= 0 and orbit_vec_x[y][x] > -1 * MIN_DIRECTION):
+            orbit_vec_x[y][x]  = -1 * MIN_DIRECTION / 2
+        if (vLength < MIN_DIRECTION / 2 and orbit_vec_y[y][x] >= 0 and orbit_vec_y[y][x] < MIN_DIRECTION):
+            orbit_vec_y[y][x]  = MIN_DIRECTION / 2
+        if (vLength < MIN_DIRECTION / 2 and orbit_vec_y[y][x] <= 0 and orbit_vec_y[y][x] > -1 * MIN_DIRECTION):
+            orbit_vec_y[y][x]  = -1 * MIN_DIRECTION / 2
+
+        vLength = vecLength(orbit_vec_x[y][x], orbit_vec_y[y][x])
 
         countAll += 1
         sumAll += vLength
-        if minAll == None or vLength < minAll:
+        if minAll is None or vLength < minAll:
             minAll = vLength
-
-# x_directions_shifted = gaussian_filter(x_directions_shifted, sigma=1)
-# y_directions_shifted = gaussian_filter(y_directions_shifted, sigma=1)
 
 print("Count", countAll)
 print("Mean", sumAll / countAll)
 print("Min", minAll)
 
-x_goals = x_directions_shifted + X
-y_goals = y_directions_shifted + Y
+orbit_goal_points_x = orbit_vec_x + X
+orbit_goal_points_y = orbit_vec_y + Y
 
-goal_map = []
+orbit_map = []
 
-for y, row in enumerate(x_goals):
-    goal_map.append([])
+for y, row in enumerate(orbit_goal_points_x):
+    orbit_map.append([])
     for x, val in enumerate(row):
-        # if(x_directions)
-        goal_map[y].append([x_goals[y][x], y_goals[y][x]])
+        orbit_map[y].append([orbit_goal_points_x[y][x], orbit_goal_points_y[y][x]])
 
 ###############################
 ##########  PICKLES  ##########
@@ -159,11 +154,7 @@ file_name = 'map_pickles/env_orbit_map.pickle'
 
 with open(file_name, 'wb') as handle:
     print('Dumping map to: ' + file_name)
-    pickle.dump(goal_map, handle, protocol=2)
-
-#     with open('goal_map.pickle', 'rb') as handle:
-#         loaded_goal_map = pickle.load(handle)
-#         print(loaded_goal_map[0][0][0] == goal_map[0][0][0])
+    pickle.dump(orbit_map, handle, protocol=2)
 
 ###############################
 ##########  FIGURES  ##########
@@ -204,7 +195,7 @@ if (SAVE_FIGURES or SHOW_FIGURES):
         plt.clf()
 
     plt.title('Gradient of Distance To Border')
-    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], xG[::GRAPH_STEP, ::GRAPH_STEP], yG[::GRAPH_STEP, ::GRAPH_STEP])
+    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], gradients_x[::GRAPH_STEP, ::GRAPH_STEP], gradients_y[::GRAPH_STEP, ::GRAPH_STEP])
     if (SAVE_FIGURES):
         plt.savefig('images/env_orbit_4_gradients_of_distance_to_border.png', dpi=1200)
     if (SHOW_FIGURES):
@@ -213,7 +204,7 @@ if (SAVE_FIGURES or SHOW_FIGURES):
         plt.clf()
 
     plt.title('Directions To Border')
-    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], x_directions[::GRAPH_STEP, ::GRAPH_STEP], y_directions[::GRAPH_STEP, ::GRAPH_STEP])
+    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], vec_to_border_x[::GRAPH_STEP, ::GRAPH_STEP], vec_to_border_y[::GRAPH_STEP, ::GRAPH_STEP])
     if (SAVE_FIGURES):
         plt.savefig('images/env_orbit_5_direction_to_border.png', dpi=1200)
     if (SHOW_FIGURES):
@@ -222,7 +213,7 @@ if (SAVE_FIGURES or SHOW_FIGURES):
         plt.clf()
 
     plt.title('Environment Orbit Directions')
-    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], x_directions_shifted[::GRAPH_STEP, ::GRAPH_STEP], y_directions_shifted[::GRAPH_STEP, ::GRAPH_STEP])
+    cp = plt.quiver(X[::GRAPH_STEP, ::GRAPH_STEP], Y[::GRAPH_STEP, ::GRAPH_STEP], orbit_vec_x[::GRAPH_STEP, ::GRAPH_STEP], orbit_vec_y[::GRAPH_STEP, ::GRAPH_STEP])
     if (SAVE_FIGURES):
         plt.savefig('images/env_orbit_6_environment_orbit_directions.png', dpi=1200)
     if (SHOW_FIGURES):
